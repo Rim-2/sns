f758d5ae83ac2a0510950123550e14796f5dc5f0
Rim-2 <turtle2260@gmail.com>
2024-02-28 11:26:31 +0900

    test: make test with mock

---
+++ src/test/java/com/fastcampus/sns/service/UserServiceTest.java
@@ -0,0 +1,89 @@
+package com.fastcampus.sns.service;
+
+import com.fastcampus.sns.exception.SnsApplicationException;
+import com.fastcampus.sns.fixture.UserEntityFixture;
+import com.fastcampus.sns.model.entity.UserEntity;
+import com.fastcampus.sns.repository.UserEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserEntityRepository userEntityRepository;
+
+    @Test
+    void 회원가입_성공() {
+        String userName = "userName";
+        String password = "password";
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(UserEntityFixture.get(userName, password)));
+
+        Assertions.assertDoesNotThrow(() -> userService.join(userName, password));
+    }
+
+    @Test
+    void 회원가입_userName_중복() {
+        String userName = "userName";
+        String password = "password";
+
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(fixture));
+
+        Assertions.assertThrows(SnsApplicationException.class, () -> userService.join(userName, password));
+    }
+
+    @Test
+    void 로그인_성공() {
+        String userName = "userName";
+        String password = "password";
+
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        Assertions.assertDoesNotThrow(() -> userService.login(userName, password));
+    }
+
+    @Test
+    void 로그인_없는_userName() {
+        String userName = "userName";
+        String password = "password";
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(SnsApplicationException.class, () -> userService.login(userName, password));
+    }
+
+    @Test
+    void 로그인_틀린_password() {
+        String userName = "userName";
+        String password = "password";
+        String wrongPassword = "wrongPassword";
+
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+
+        // mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+
+        Assertions.assertThrows(SnsApplicationException.class, () -> userService.login(userName, wrongPassword));
+    }
+}
